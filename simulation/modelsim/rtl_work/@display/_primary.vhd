39ef53f759ac9f1213620da728fdfebfb9d7729e
hericovaliati <hericovaliati@localhost>
2013-06-10 17:05:02 -0300

    1.01

--- simulation/modelsim/rtl_work/@display/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity Display is
-    port(
-        \out\           : out    vl_logic_vector(6 downto 0);
-        \in\            : in     vl_logic_vector(3 downto 0)
-    );
-end Display;
